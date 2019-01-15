995555f43039a624fd6751110b90e7144099af0c
Hitesh Yadav <hhiteshyadav@gmail.com>
2019-01-15 13:21:41 +0530

    Update app.js

--- 2-routing/app.js
+++ 2-routing/app.js
@@ -1,12 +1,52 @@
 var express = require('express');
+const bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 
 app.listen(3000)
 
-// Routing for about, create a view at views/about.ejs in HTML
+// Other option also for routing
+// const router = express.Router() // 1
+
+// Routing for Homepage, create a view at views/home.ejs using HTML
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+// Routing for about, create a view at views/about.ejs using HTML
 app.get('/about', function(req, res) {
   res.render('about');
+});
+
+// Routing for sending query through URL
+app.get('/users/:name', function (req, res) {
+  res.send(`Welcome, ${req.params.name}!`)
+})
+
+// Routing for POST Requests, try this in POSTMAN
+app.post('/contact', function (req, res) {
+  res.send('Page with POST request method')
+})
+
+// Routing for handling all kind of requests
+app.all('/services', function (req, res) {
+  res.send('Page with All kind of request method')
+})
+
+// Login Page
+app.route('/login')
+ .get((req, res) => {
+   res.render('login')
+ })
+ .post((req, res) => {
+   const postBody = req.body;
+   res.send(postBody.username);
+ })
+
+// Routing for Error Page, create a view at views/error/404.ejs using HTML
+app.get('*', function(req, res) {
+  res.render('error/404');
 });
 
 
